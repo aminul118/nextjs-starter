069ae0e354be19da8569ba43c140a9470adb1204
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-08-27 03:03:31 +0600

    Footer customization

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -2,70 +2,104 @@
 import Container from '@/components/ui/container';
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
-interface Footer7Props {
-  logo?: {
-    url: string;
-    src: string;
-    alt: string;
-    title: string;
-  };
-  sections?: Array<{
-    title: string;
-    links: Array<{ name: string; href: string }>;
-  }>;
-  description?: string;
-  socialLinks?: Array<{
-    icon: React.ReactElement;
-    href: string;
-    label: string;
-  }>;
-  copyright?: string;
-  legalLinks?: Array<{
-    name: string;
-    href: string;
-  }>;
-}
-
-const defaultSections = [
+const sections = [
   {
     title: 'Product',
     links: [
-      { name: 'Overview', href: '#' },
-      { name: 'Pricing', href: '#' },
-      { name: 'Marketplace', href: '#' },
-      { name: 'Features', href: '#' },
+      {
+        name: 'Overview',
+        href: '#',
+      },
+      {
+        name: 'Pricing',
+        href: '#',
+      },
+      {
+        name: 'Marketplace',
+        href: '#',
+      },
+      {
+        name: 'Features',
+        href: '#',
+      },
     ],
   },
   {
     title: 'Company',
     links: [
-      { name: 'About', href: '#' },
-      { name: 'Team', href: '#' },
-      { name: 'Blog', href: '#' },
-      { name: 'Careers', href: '#' },
+      {
+        name: 'About',
+        href: '#',
+      },
+      {
+        name: 'Team',
+        href: '#',
+      },
+      {
+        name: 'Blog',
+        href: '#',
+      },
+      {
+        name: 'Careers',
+        href: '#',
+      },
     ],
   },
   {
     title: 'Resources',
     links: [
-      { name: 'Help', href: '#' },
-      { name: 'Sales', href: '#' },
-      { name: 'Advertise', href: '#' },
-      { name: 'Privacy', href: '#' },
+      {
+        name: 'Help',
+        href: '#',
+      },
+      {
+        name: 'Sales',
+        href: '#',
+      },
+      {
+        name: 'Advertise',
+        href: '#',
+      },
+      {
+        name: 'Privacy',
+        href: '#',
+      },
     ],
   },
 ];
 
-const defaultSocialLinks = [
-  { icon: <FaInstagram className="size-5" />, href: '#', label: 'Instagram' },
-  { icon: <FaFacebook className="size-5" />, href: '#', label: 'Facebook' },
-  { icon: <FaTwitter className="size-5" />, href: '#', label: 'Twitter' },
-  { icon: <FaLinkedin className="size-5" />, href: '#', label: 'LinkedIn' },
+const socialLinks = [
+  {
+    icon: <FaInstagram className="size-5" />,
+    href: '#',
+    label: 'Instagram',
+  },
+  {
+    icon: <FaFacebook className="size-5" />,
+    href: '#',
+    label: 'Facebook',
+  },
+  {
+    icon: <FaTwitter className="size-5" />,
+    href: '#',
+    label: 'Twitter',
+  },
+  {
+    icon: <FaLinkedin className="size-5" />,
+    href: '#',
+    label: 'LinkedIn',
+  },
 ];
 
-const defaultLegalLinks = [
-  { name: 'Terms and Conditions', href: '#' },
-  { name: 'Privacy Policy', href: '#' },
+const legalLinks = [
+  {
+    name: 'Terms and Conditions',
+    href: '/terms-conditions',
+  },
+  {
+    name: 'Privacy Policy',
+    href: '/privacy-policy',
+  },
 ];
 
 const Footer = ({
@@ -75,12 +109,8 @@ const Footer = ({
     alt: 'logo',
     title: 'Shadcnblocks.com',
   },
-  sections = defaultSections,
   description = 'A collection of components for your startup business or side project.',
-  socialLinks = defaultSocialLinks,
-  copyright = '© 2024 Shadcnblocks.com. All rights reserved.',
-  legalLinks = defaultLegalLinks,
-}: Footer7Props) => {
+}) => {
   return (
     <Container backgroundColor="border-t">
       <div className="flex w-full flex-col justify-between gap-10 lg:flex-row lg:items-start lg:text-left">
@@ -126,7 +156,11 @@ const Footer = ({
         </div>
       </div>
       <div className="text-muted-foreground mt-8 flex flex-col justify-between gap-4 border-t py-8 text-xs font-medium md:flex-row md:items-center md:text-left">
-        <p className="order-2 lg:order-1">{copyright}</p>
+        <p className="order-2 lg:order-1">
+          &copy; {new Date().getFullYear()} Your Company Name. All rights
+          reserved.
+        </p>
+
         <ul className="order-1 flex flex-col gap-2 md:order-2 md:flex-row">
           {legalLinks.map((link, idx) => (
             <li key={idx} className="hover:text-primary">
